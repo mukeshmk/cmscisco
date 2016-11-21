2858807c7aadbe6692fb224a69cc2bb96e3d31e7
chan24 <aakaschan24@gmail.com>
2016-11-21 18:12:39 +0530

    Src files

---
+++ app/app.routing.ts
@@ -0,0 +1,45 @@
+import {ModuleWithProviders} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {OrdercountComponent} from  './ordercount/ordercount.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { OrdersComponent } from './orders/orders.component';
+import { RealignmentComponent } from './realignment/realignment.component';
+
+
+
+const appRoutes=[
+  {
+    path : '',
+    name: 'OrderCount',
+    component : OrdercountComponent,
+  },
+  {
+    path : 'dashboard',
+    name: 'Dashboard',
+    component : DashboardComponent
+  },
+  {
+    path : 'accounts',
+    name: 'Accounts',
+    component : AccountsComponent
+  },
+  {
+    path : 'organization',
+    name: 'Organization',
+    component : OrganizationComponent
+  },
+  {
+    path : 'orders',
+    name: 'Orders',
+    component : OrdersComponent
+  },
+  {
+    path : 'CMTAlignment',
+    name: 'CMTAlignment',
+    component : RealignmentComponent
+  },
+];
+
+export const Routing : ModuleWithProviders = RouterModule.forRoot(appRoutes);
---
+++ app/ordercount/ordercount.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ordercount',
+  templateUrl: './ordercount.component.html',
+  styleUrls: ['./ordercount.component.css']
+})
+export class OrdercountComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
