ee4924c9a646f69ce1748ce4b3e18fb556181110
chan24 <aakashchan24@gmail.com>
2016-11-24 03:19:10 +0530

    view organization works

--- app/view-organization/view-organization.component.ts
+++ app/view-organization/view-organization.component.ts
@@ -8,19 +8,30 @@ import 'rxjs/Rx';
   templateUrl: './view-organization.component.html',
   styleUrls: ['./view-organization.component.css'],
 })
+
 export class ViewOrganizationComponent implements OnInit {
 
 
-  resultJson: any;
+  ifChecked : boolean;
+  public resultSet : any[];
 
 
+  generateArray(obj){
+    return Object.keys(obj).map( (key) => {return obj[key]});
+  }
+
+  clickAll(ifChecked){
+      if(ifChecked == true){
+
+      }
+  }
 
   constructor(http: Http) {
     console.log("json-server called");
 
-    http.get('http://localhost:3005/db')
+    http.get('http://localhost:3000/users')
         .map(response => response.json())
-        .subscribe(result => this.resultJson =result);
+        .subscribe(result => this.resultSet = result);
   }
 
   ngOnInit() {
