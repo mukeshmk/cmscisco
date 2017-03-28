5d40149969af58e2070acff12d71799cd374ee3a
chan24 <aakashchan24@gmail.com>
2017-03-28 21:12:53 +0530

    blis routes fixed

--- src/app/blis/realign/realign-view/realign-view.component.spec.ts
+++ src/app/blis/realign/realign-view/realign-view.component.spec.ts
@@ -1,6 +1,5 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
 import { RealignViewComponent } from './realign-view.component';
 
 describe('Component: RealignView', () => {
